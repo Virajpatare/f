239b7e902dacab4643e8242a3693e24ee7fba1fb
Virajpatare <virajpatare02@gmail.com>
2025-04-03 15:34:32 +0530

    Update two.java

--- two.java
+++ two.java
@@ -18,7 +18,20 @@ return a+b;
 
 system.out.println("addition is : "+addition(10,20));
 
+public int multiply(int a, int b){
+  return a*b;
+}
 
+system.out.println("multiplication is : "+multiplication(10,20));
+
+
+
+
+
+
+
+  9
+  
 
 
 
