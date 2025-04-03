0dc24ebb21db488478602f2b7e5c3b5d377b95b0
viraj <viraj@gmail.com>
2025-04-03 15:30:20 +0530

    mkm

---
+++ two.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class Addition{
+
+public static void main(string []args){
+
+
+public int addition(int a,int b){
+
+
+return a+b;
+
+
+
+}
+
+system.out.println("addition is : "+addition(10,20));
+
+
+
+
+
+
+
+
+}
